8c018abb9682fedf35b9553e4f2d2bd57aa1a752
AlikOsta <167987940+AlikOsta@users.noreply.github.com>
2025-07-05 18:38:45 -0300

    Изменил шаблон категории и поиск исправил

--- ework_rubric/models.py
+++ ework_rubric/models.py
@@ -29,6 +29,7 @@ class SuperRubric(models.Model):
 
 class SubRubric(models.Model):
     name = models.CharField(max_length=30, db_index=True, verbose_name=_('Название'), help_text=_('Название подрубрики'))
+    icon = models.ImageField(upload_to='icons', blank=True, null=True, verbose_name=_('Иконка'), help_text=_('Иконка подрубрики'))
     slug = models.SlugField(max_length=50, unique=True, db_index=True, verbose_name=_('Слаг'), help_text=_('Слаг подрубрики'))
     order = models.SmallIntegerField(default=0, db_index=True, verbose_name=_('Порядок'), help_text=_('Порядок подрубрики'))
     super_rubric = models.ForeignKey(SuperRubric, on_delete=models.PROTECT, related_name='sub_rubrics', verbose_name=_('Категория'), help_text=_('Категория подрубрики'))
