a8775ec58b8cc9c4922555817063ef2b427f5078
AlikOsta <167987940+AlikOsta@users.noreply.github.com>
2025-07-21 09:07:20 -0300

    чистка

--- ework_payment/urls.py
+++ ework_payment/urls.py
@@ -1,11 +1,9 @@
 # urls.py
 from django.urls import path
-from .views import create_payment
 from .webhook_handlers import TelegramPaymentWebhookView
 
 app_name = 'payment'
 
 urlpatterns = [
-    path('create_payment/', create_payment, name='create_payment'),
     path('telegram-webhook/', TelegramPaymentWebhookView.as_view(), name='telegram_webhook'),
 ]
