38437ca7aa324d11955ac2a354b1e74689f5d509
AlikOsta <167987940+AlikOsta@users.noreply.github.com>
2025-07-21 23:32:45 -0300

    Список городов

--- ework_locations/models.py
+++ ework_locations/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
-
 class City(models.Model):
     name = models.CharField(max_length=50, db_index=True, verbose_name=_("Название города"), help_text=_("Название города"))
     order = models.SmallIntegerField(default=0, db_index=True, verbose_name=_("Порядок"), help_text=_("Порядок города"))
