eb4b1837455cb1afd4f50009cfe8c6d610f1ed4b
AlikOsta <167987940+AlikOsta@users.noreply.github.com>
2025-09-07 22:02:08 -0300

    вернул кнопки

--- ework_bot_tg/bot/bot.py
+++ ework_bot_tg/bot/bot.py
@@ -92,21 +92,10 @@ async def send_telegram_city_chat(data):
 
         keyboard = InlineKeyboardMarkup(
             inline_keyboard=[
-                [
-                    InlineKeyboardButton(
-                        text="Розмістити вакансію на HW",
-                        web_app=WebAppInfo(url=cfg['miniapp_url'])
-                    )
-                ],
-                [
-                    InlineKeyboardButton(
-                        text="Переглянути інші вакансії",
-                        web_app=WebAppInfo(url=cfg['miniapp_url'])
-                    )
-                ]
+                [InlineKeyboardButton(text="Розмістити вакансію на HW", url=f"t.me/HelpWorkUaBoT")],
+                [InlineKeyboardButton(text="Переглянути інші вакансії ", url=f"t.me/HelpWorkUaBoT")]
             ]
         )
-        
         try:
             await bot.send_photo(chat_id=chat_id, photo=photo_url, caption=message, reply_markup=keyboard)
         except Exception as e:
